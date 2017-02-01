bea6aafb86d1809271dcf7fe4158037cc3617b0e
taichi.amano <taichi.amano@speee.jp>
2017-02-01 18:48:36 +0900

    Add endpoints relation and validations to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  has_many :endpoints, dependent: :destroy
+
+  validates :arn, presence: true
+  validates :repository, presence: true, uniqueness: { scope: :pr_number }
+  validates :pr_number, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :endpoints, presence: true
 end
