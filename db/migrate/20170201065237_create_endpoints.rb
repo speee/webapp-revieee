9bebe48ab5d925f5514f7188599f72a60a5bb529
taichi.amano <taichi.amano@speee.jp>
2017-02-08 17:17:44 +0900

    Remove subdomain column in endpoints from create_table sentence

--- db/migrate/20170201065237_create_endpoints.rb
+++ db/migrate/20170201065237_create_endpoints.rb
@@ -1,13 +1,11 @@
 class CreateEndpoints < ActiveRecord::Migration[5.0]
   def change
     create_table :endpoints, unsigned: true do |t|
-      t.string :subdomain, limit: 32, null: false
       t.string :ip, limit: 15, null: false
       t.integer :port, limit: 2, unsigned: true, null: false
       t.belongs_to :task, foreign_key: true, unsigned: true, null: false
 
       t.timestamps
     end
-    add_index :endpoints, :subdomain, unique: true
   end
 end
