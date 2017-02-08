7a740aaf7e203d40a20aa6fb697919bb9073cbdb
taichi.amano <taichi.amano@speee.jp>
2017-02-08 18:33:15 +0900

    Update ip format validation in Endpoint

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -1,11 +1,11 @@
 class Endpoint < ApplicationRecord
-  IP_REGEXP = /\A(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\z/.freeze
   SUBDOMAIN_PREFIX = 'endpoint'.freeze
 
   belongs_to :task
 
-  validates :ip, presence: true, format: { with: IP_REGEXP }
+  validates :ip, presence: true
   validates :port, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 65535 }
+  validate :validate_ip_format
 
   def url
     "http://#{domain}/"
@@ -13,5 +13,15 @@ class Endpoint < ApplicationRecord
 
   def domain
     "#{SUBDOMAIN_PREFIX}-#{id}.#{Settings.domain}"
+  end
+
+  private
+
+  def validate_ip_format
+    valid = IPAddr.new(ip).ipv4?
+  rescue IPAddr::InvalidAddressError
+    valid = false
+  ensure
+    errors.add(:ip, 'IPの形式が不正です') unless valid
   end
 end
