061bc65e51e8df8ae1114b7d7bc1c21f59b592b6
Morioka Shuhei <selmertsx@gmail.com>
2017-02-18 12:19:18 +0900

    error handling in spot_fleet_request.create

--- app/models/spot_fleet_request.rb
+++ app/models/spot_fleet_request.rb
@@ -3,9 +3,14 @@ module SpotFleetRequest
   def self.create
     spot_instance = SpotFleetRequest::SpotInstance.new
     request_config = SpotFleetRequest::RequestConfig.new(spot_instance: spot_instance)
-    ec2_client = Aws::EC2::Client.new(region: Settings.aws.region)
-    response = ec2_client.request_spot_fleet(request_config.to_hash)
 
+    begin
+      ec2_client = Aws::EC2::Client.new(region: Settings.aws.region)
+      response = ec2_client.request_spot_fleet(request_config.to_hash)
+      Rails.logger.info "spot fleet request is accepted! request_id: #{response.spot_fleet_request_id}"
+    rescue => e
+      Rails.logger.error "spot fleet request is denied! error message: #{e.message}"
+    end
   end
 
   def describe
