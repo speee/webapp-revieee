69709537c6f0a1bd7d2e937922fa1a158e7bfb17
Morioka Shuhei <selmertsx@gmail.com>
2017-02-22 11:51:02 +0900

    logging in spot_fleet_request/client describe, cancel method

--- app/models/spot_fleet_request/client.rb
+++ app/models/spot_fleet_request/client.rb
@@ -15,11 +15,13 @@ module SpotFleetRequest
     end
 
     def describe(spot_fleet_request_id)
-      ec2.describe_spot_fleet_requests({ spot_fleet_request_ids: [ spot_fleet_request_id ] })
+      response = ec2.describe_spot_fleet_requests({ spot_fleet_request_ids: [ spot_fleet_request_id ] })
+      Rails.logger.info response
     end
 
     def cancel(spot_fleet_request_id)
-      ec2.cancel_spot_fleet_requests({ spot_fleet_request_ids: [ spot_fleet_request_id ], terminate_instances: true })
+      response = ec2.cancel_spot_fleet_requests({ spot_fleet_request_ids: [ spot_fleet_request_id ], terminate_instances: true })
+      Rails.logger.info response
     end
   end
 end
