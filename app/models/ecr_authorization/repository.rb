1cc4340473e624feaf9c84e7eb4cbd0fb833b252
Morioka Shuhei <selmertsx@gmail.com>
2017-03-07 20:25:32 +0900

    Creating ECR Repository with repository_name

--- app/models/ecr_authorization/repository.rb
+++ app/models/ecr_authorization/repository.rb
@@ -6,5 +6,11 @@ module ECRAuthorization
     def initialize(name)
       @name = name
     end
+
+    def create
+      response = ecs.create_repository({ repository_name: @name })
+      @registry_id = response.repository.registry_id
+      self
+    end
   end
 end
