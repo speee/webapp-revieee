f3bec713218a9648f450926a9fe05c35494f750e
Morioka Shuhei <selmertsx@gmail.com>
2017-03-07 20:48:00 +0900

    Allowing user access to ECR Repository

--- app/models/ecr_authorization/repository.rb
+++ app/models/ecr_authorization/repository.rb
@@ -12,5 +12,38 @@ module ECRAuthorization
       @registry_id = response.repository.registry_id
       self
     end
+
+    def allow_access(user_arn)
+      ecs.set_repository_policy({
+        registry_id: @registry_id,
+        repository_name: @name,
+        policy_text: policy_text(user_arn),
+        force: false,
+      })
+    end
+
+    private
+
+    def policy_text(user_arn)
+      {
+        Version: '2008-10-17',
+        Statement: [
+          {
+            Sid: 'AllowPushPull',
+            Effect: 'Allow',
+            Principal: { AWS: [ user_arn ] },
+            Action: [
+              'ecr:GetDownloadUrlForLayer',
+              'ecr:BatchGetImage',
+              'ecr:BatchCheckLayerAvailability',
+              'ecr:PutImage',
+              'ecr:InitiateLayerUpload',
+              'ecr:UploadLayerPart',
+              'ecr:CompleteLayerUpload'
+            ]
+          }
+        ]
+      }.to_json
+    end
   end
 end
