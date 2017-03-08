9712f1dc1ac20b0ec33de98039bcab0c6c937575
Morioka Shuhei <selmertsx@gmail.com>
2017-03-08 09:57:31 +0900

    Creating access key for ecr_authorization/iam_user

--- app/models/ecr_authorization/iam_user.rb
+++ app/models/ecr_authorization/iam_user.rb
@@ -11,6 +11,10 @@ module ECRAuthorization
       iam.create_user({ user_name: @name })
     end
 
+    def create_access_key
+      iam.create_access_key({ user_name: @name })
+    end
+
     def join(group)
       iam.add_user_to_group({
         group_name: group.name,
