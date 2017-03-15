3f5b60778a37aae99510e124e599feaeca87798e
Morioka Shuhei <selmertsx@gmail.com>
2017-03-15 10:46:17 +0900

    rename ecr_authorization to ecr
    Since ecr_authorization now has the role of creating iam_user and iam_group.

--- app/models/ecr/iam_group.rb
+++ app/models/ecr/iam_group.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-module ECRAuthorization
+module Ecr
   class IamGroup
     include AwsAccessible
     attr_reader :name
--- app/models/ecr/repository.rb
+++ app/models/ecr/repository.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-module ECRAuthorization
+module Ecr
   class Repository
     include AwsAccessible
 
