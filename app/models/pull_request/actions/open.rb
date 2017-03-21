c677346d715adc564348452d5be9cbbd8093add6
taichi.amano <taichi.amano@speee.jp>
2017-03-21 11:11:03 +0900

    Commonalize commenting to github

--- app/models/pull_request/actions/open.rb
+++ app/models/pull_request/actions/open.rb
@@ -2,11 +2,7 @@ class PullRequest::Actions::Open < PullRequest::Actions
   def handle
     return unless target.create
 
-    PullRequest::Comment.new.add(
-      repository: payload.head_repository,
-      pr_number: payload.pr_number,
-      message: message,
-    )
+    add_comment(message)
   end
 
   private
