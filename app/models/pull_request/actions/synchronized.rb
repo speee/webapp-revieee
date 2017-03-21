63768c86b584277a407013fbdb6a721f1d753620
taichi.amano <taichi.amano@speee.jp>
2017-03-21 11:13:06 +0900

    Comment to github when syncronized action called

--- app/models/pull_request/actions/synchronized.rb
+++ app/models/pull_request/actions/synchronized.rb
@@ -3,5 +3,13 @@ class PullRequest::Actions::Synchronized < PullRequest::Actions
     return unless target.update
 
     Net::HTTP.get URI.parse(target.cache_clear_url)
+
+    add_comment(message)
+  end
+
+  private
+
+  def message
+    target.task.endpoints.map(&:url).join("\n")
   end
 end
