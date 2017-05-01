8a43dd6a1df4685d5a179fa89baad992bbea407e
Morioka Shuhei <selmertsx@gmail.com>
2017-05-01 10:24:08 +0900

    Clearing nginx url cache when close PR
    nginx url cache should be cleared when PR is updated, or closed.

--- app/models/pull_request/actions/close.rb
+++ app/models/pull_request/actions/close.rb
@@ -2,6 +2,10 @@ class PullRequest::Actions::Close < PullRequest::Actions
   def handle
     return unless target.delete
 
+    target.cache_clear_urls.each do |url|
+      Net::HTTP.get URI.parse(url)
+    end
+
     add_comment('ReviewAppsを削除しました')
   end
 end
