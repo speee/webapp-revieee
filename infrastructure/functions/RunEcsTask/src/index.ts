31d1e2a55259ba9c29e0a667d5bb077e1383001c
taichi.amano <taichi.amano@speee.jp>
2017-07-21 06:01:43 +0900

    [CloudFormation] Add MySQL Client to RunEcsTaskFunction

--- infrastructure/functions/RunEcsTask/src/index.ts
+++ infrastructure/functions/RunEcsTask/src/index.ts
@@ -1,7 +1,27 @@
 import { Callback, Context } from "aws-lambda";
+import { createConnection, Connection } from "typeorm";
+import "reflect-metadata";
 import { ApiGatewayEvent } from "./ApiGatewayEvent";
 
+async function buildConnection(): Promise<Connection> {
+    return createConnection({
+        "driver": {
+            "type": "mysql",
+            "host": process.env.TYPEORM_HOST,
+            "port": process.env.TYPEORM_PORT,
+            "username": process.env.TYPEORM_USERNAME,
+            "password": process.env.TYPEORM_PASSWORD,
+            "database": process.env.TYPEORM_DATABASE,
+        },
+        "entities": [
+            __dirname + "/entity/*.js"
+        ]
+    });
+}
 
 
 export function handler(event: ApiGatewayEvent, context: Context, callback: Callback) {
+    (async () => {
+        const connection = await buildConnection();
+    })();
 }
