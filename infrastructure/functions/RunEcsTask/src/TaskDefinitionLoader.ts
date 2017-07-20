3d7abac1bd4340ad33221391500b1456b1dd39dd
taichi.amano <taichi.amano@speee.jp>
2017-07-21 06:20:36 +0900

    [CloudFormation] Create TaskDefinition if not found

---
+++ infrastructure/functions/RunEcsTask/src/TaskDefinitionLoader.ts
@@ -0,0 +1,46 @@
+import { ECS } from "aws-sdk";
+import * as GitHub from "github-api";
+import * as YAML from "js-yaml";
+import { RevieeeTarget } from "./entity/RevieeeTarget";
+
+export class TaskDefinitionLoader {
+    readonly configFile = "task_definition.yml"
+    revieeeTarget: RevieeeTarget;
+
+    constructor(revieeeTarget: RevieeeTarget) {
+        this.revieeeTarget = revieeeTarget;
+    }
+
+    async load(): Promise<ECS.RegisterTaskDefinitionRequest> {
+        const gh = new GitHub({ token: process.env.GITHUB_ACCESS_TOKEN })
+        const repository = gh.getRepo(this.revieeeTarget.repository)
+        return repository.getContents(
+            this.revieeeTarget.branch,
+            this.configFile,
+        ).then(async result => {
+            return await this.parseContent(result.data.content);
+        });
+    }
+
+    private async parseContent(content: string): Promise<ECS.RegisterTaskDefinitionRequest> {
+        const parsedContent = new Buffer(content, 'base64').toString();
+        const json = YAML.safeLoad(parsedContent);
+        return this.camelizeKeys(json);
+    }
+
+    private camelizeKeys = (obj) => {
+        if (!obj || typeof obj !== 'object') return obj;
+        if (Array.isArray(obj)) return obj.map(this.camelizeKeys);
+        const newObj = {};
+        Object.keys(obj).forEach((key: string) => {
+            newObj[this.camelizeStr(key)] = this.camelizeKeys(obj[key]);
+        });
+        return newObj;
+    }
+
+    private camelizeStr = (str: string): string => {
+        return str.replace(/[_](\w|$)/g, (_, x) => {
+            return x.toUpperCase();
+        });
+    }
+}
--- infrastructure/functions/RunEcsTask/src/entity/RevieeeTarget.ts
+++ infrastructure/functions/RunEcsTask/src/entity/RevieeeTarget.ts
@@ -1,5 +1,8 @@
+import { ECS } from "aws-sdk";
 import { Repository } from "typeorm";
 import { TaskDefinition } from "./TaskDefinition";
+import { TaskDefinitionLoader } from "../TaskDefinitionLoader";
+
 export class RevieeeTarget {
     repository: string;
     branch: string;
@@ -18,7 +21,24 @@ export class RevieeeTarget {
             return this.taskDefinition;
         }
         let taskDefinition = await this.taskDefinitionRepository.findOne({ repository: this.repository });
+        if (taskDefinition === undefined) {
+            taskDefinition = await this.createTaskDefinition();
+        }
         this.taskDefinition = taskDefinition;
         return this.taskDefinition;
+    }
+
+    private async createTaskDefinition(): Promise<TaskDefinition> {
+        const ecs = new ECS();
+        const taskDefinitionLoader = new TaskDefinitionLoader(this);
+        const taskDefinitionConfig = await taskDefinitionLoader.load();
+        return ecs.registerTaskDefinition(taskDefinitionConfig).promise()
+            .then((result: AWS.ECS.RegisterTaskDefinitionResponse) => {
+                const taskDefinition = new TaskDefinition();
+                taskDefinition.name = result.taskDefinition.family;
+                taskDefinition.repository = this.repository;
+                this.taskDefinitionRepository.persist(taskDefinition);
+                return taskDefinition;
+            });
     }
 }
